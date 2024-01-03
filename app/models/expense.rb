aab7f4e46af0f73d8961824e3ecb9cc23fb5ec9d
Augusto-Carissimo <augusto.carissimo@gmail.com>
2024-01-02 22:18:32 -0300

    Add Expense.cost default and validations

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,4 +2,6 @@ class Expense < ApplicationRecord
   scope :current_month, -> { where('created_at > ?', (DateTime.current - 1.months)) }
 
   belongs_to :type
+
+  validates :cost, numericality: { greater_than_or_equal_to: 0 }
 end
