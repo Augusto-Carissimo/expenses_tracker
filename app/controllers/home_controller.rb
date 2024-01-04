3af24dc9a40f98f22b114c49063256d2b065dc0c
Augusto-Carissimo <augusto.carissimo@gmail.com>
2024-01-04 17:56:40 -0300

    Refactor Type total_expenses and monthly_expenses

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -18,8 +18,10 @@ class HomeController < ApplicationController
   def total_expenses
     total_expense = Type.all.map(&:total_expense).sum
     expense = {}
-    Type.all.each do |type|
-      expense[type.name] = ((type.total_expense / total_expense)*100).round(2)
+    if total_expense > 0
+      Type.all.each do |type|
+        expense[type.name] = ((type.total_expense / total_expense)*100).round(2)
+      end
     end
     expense
   end
@@ -27,8 +29,10 @@ class HomeController < ApplicationController
   def monthly_expenses
     total_monthy_expense = Type.all.map(&:month_expense).sum
     expense = {}
-    Type.all.each do |type|
-      expense[type.name] = ((type.month_expense / total_monthy_expense)*100).round(2)
+    if total_monthy_expense > 0
+      Type.all.each do |type|
+        expense[type.name] = ((type.month_expense / total_monthy_expense)*100).round(2)
+      end
     end
     expense
   end
